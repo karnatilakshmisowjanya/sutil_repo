29a400570be364e0c9d93d695727a7b6dee66f44
Kajal Sawale <ksawale@slb.com>
2024-01-03 19:34:12 +0000

    feat: Added support for cold tier for uploading dataset

--- sdlib/cmd/cp/cmd.py
+++ sdlib/cmd/cp/cmd.py
@@ -169,7 +169,7 @@ class Cp(SDUtilCMD):
         storage_tier = None
 
         if keyword_args.tier is not None:
-            if str(keyword_args.tier).capitalize() not in ('Hot', 'Cool', 'Archive'):
+            if str(keyword_args.tier).capitalize() not in ('Hot', 'Cool', 'Archive', 'Cold'):
                 raise Exception(f'\'{keyword_args.tier}\' is not an acceptable Storage tier')
             storage_tier = str(keyword_args.tier).capitalize()
         tier = Tier(storage_tier)
