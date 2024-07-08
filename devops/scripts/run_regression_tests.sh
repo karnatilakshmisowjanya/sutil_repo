43d9a4710b86c6b39b22c306da833a2fde0f368b
Konstantin Khottchenkov <kkhottchenkov@slb.com>
2024-07-08 12:27:03 +0200

    ci: print error

--- devops/scripts/run_regression_tests.sh
+++ devops/scripts/run_regression_tests.sh
@@ -121,9 +121,10 @@ echo "auth_provider:" >> sdlib/config.yaml
 echo "  default: null" >> sdlib/config.yaml
 
 # pytest fetches a stoken when a service account secret key is passed.
-pytest --forked --log-format="%(asctime)s %(levelname)s %(message)s" --log-date-format="%Y-%m-%d %H:%M:%S" --timeout=300 --capture=fd \
-    test/e2e --idtoken=${idtoken} --sdpath=sd://${tenant}/${subproject} --admin=${admin} --legaltag=${legaltag} --legaltag=${legaltag02} --acl_admin=${acl_admin} --acl_viewer=${acl_viewer}
+error = $(pytest --forked --log-format="%(asctime)s %(levelname)s %(message)s" --log-date-format="%Y-%m-%d %H:%M:%S" --timeout=300 --capture=fd \
+    test/e2e --idtoken=${idtoken} --sdpath=sd://${tenant}/${subproject} --admin=${admin} --legaltag=${legaltag} --legaltag=${legaltag02} --acl_admin=${acl_admin} --acl_viewer=${acl_viewer} 2>&1)
 exit_status=$?
+if [ $exit_status -ne 0 ]; then echo $error; fi
 
 # restore configuration and clear temporary files
 FILE2=config_original.yaml
