5ecacdb42afa8dc148d3ef0c724bd4afafab9512
Konstantin Khottchenkov <kkhottchenkov@slb.com>
2024-07-08 14:50:00 +0200

    test: review patch command test

--- test/e2e/test_1_subproject.py
+++ test/e2e/test_1_subproject.py
@@ -50,21 +50,22 @@ def test_sdutil_ls_subprojects(capsys, pargs):
     sdutil_list_status, sdutil_list_output = run_command(capsys)
     assert not sdutil_list_status, sdutil_list_output
 
-# def test_sdutil_patch_subproject(capsys, pargs):
-#     import json
-#     path = pargs.sdpath
-#     tenant,subproject = path.split("/")[2],path.split("/")[3]
-#     legaltag02 = pargs.legaltag02
-#     set_args("patch {path} --ltag={ltag} --idtoken={stoken}".format(path=path, ltag=legaltag02, stoken=pargs.idtoken))
-#     sdutil_patch_status, sdutil_patch_output = run_command(capsys)
-#     time.sleep(10)
-#     response = subproject_get(tenant, subproject, pargs.idtoken)
-#     subroject_meta = json.loads(response.content)
-#     subproject_patch_status = 0 if subroject_meta['ltag'] == legaltag02 else 1
-#     errors = verify_conditions( sdutil_patch_subproject = str(sdutil_patch_status) + ';' + sdutil_patch_output,
-#                                 subroject_get_after_sdutil_rm_subproject = str(subproject_patch_status) + ';' + 'Subproject was not patched after sdutil patch command\n' + 'ltag: ' + subroject_meta['ltag']
-#                             )
-#     assert not errors, "errors occured:\n{}".format("\n".join(errors))
+def test_sdutil_patch_subproject(capsys, pargs):
+    import json
+    import sys
+    path = pargs.sdpath
+    tenant,subproject = path.split("/")[2],path.split("/")[3]
+    legaltag02 = pargs.legaltag02
+    set_args("patch {path} --ltag={ltag} --idtoken={stoken}".format(path=path, ltag=legaltag02, stoken=pargs.idtoken))
+    sdutil_patch_status, sdutil_patch_output = run_command(capsys)
+    time.sleep(10)
+    response = subproject_get(tenant, subproject, pargs.idtoken)
+    subroject_meta = json.loads(response.content)
+    subproject_patch_status = 0 if subroject_meta['ltag'] == legaltag02 else 1
+    errors = verify_conditions( sdutil_patch_subproject = str(sdutil_patch_status) + ';' + sdutil_patch_output + 'Command: ' + sys.argv,
+                                subroject_get_after_sdutil_rm_subproject = str(subproject_patch_status) + ';' + 'Subproject was not patched after sdutil patch command\n' + 'ltag: ' + subroject_meta['ltag']
+                            )
+    assert not errors, "errors occured:\n{}".format("\n".join(errors))
 
 def test_sdutil_rm_subproject(capsys, pargs):
     path = pargs.sdpath
